5a05a2bbb725946d89e0d2cef0da863fb5a388d3
josue santos <josue.santos.dev@gmail.com>
2025-08-10 02:35:59 -0300

    added a commit pkg

---
+++ pkg/commit.go
@@ -0,0 +1,25 @@
+package pkg
+
+type Commit interface {
+	Apply() error
+}
+
+type Commit struct {
+	ID      string
+	Event   string
+	Payload any
+	CheckSum string
+}
+
+func NewCommit(id string, event string, payload any) *Commit {
+	return &Commit{
+		ID:      id,
+		Event:   event,
+		Payload: payload,
+	}
+}
+
+func (c *Commit) Apply() Commit {
+	c.CheckSum = fmt.Sprintf("%x", sha256.Sum256([]byte(fmt.Sprintf("%s%s%v", c.ID, c.Event, c.Payload))))
+	return *c
+}
