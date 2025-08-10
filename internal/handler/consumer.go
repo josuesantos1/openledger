b817cee551b7c0f39bd38ae8d93693237ce87c2e
josue santos <josue.santos.dev@gmail.com>
2025-08-10 12:24:14 -0300

    improve impl from queue handler and added simple consumer handler

--- internal/handler/consumer.go
+++ internal/handler/consumer.go
@@ -3,46 +3,61 @@ package handler
 import (
 	"log"
 	"github.com/josuesantos1/openledger/pkg"
+	amqp091 "github.com/rabbitmq/amqp091-go"
 )
 
 type ConsumerHandler struct {
 	storage *pkg.Storage
+	queue   *pkg.RabbitMQ
 }
 
-func NewConsumerHandler(storage *pkg.Storage) *ConsumerHandler {
+func NewConsumerHandler(storage *pkg.Storage, queue *pkg.RabbitMQ) *ConsumerHandler {
 	return &ConsumerHandler{
 		storage: storage,
+		queue:   queue,
 	}
 }
 
-func (h *ConsumerHandler) RegisterConsumers(queue *pkg.RabbitMQ) error {
-	q, err := queue.QueueDeclare("openledger.transaction", false, false, false, false, nil)
-	if err != nil {
-		return err
+func (h *ConsumerHandler) RegisterConsumers() error {
+	queues := map[string]func(amqp091.Delivery){
+		"openledger.transaction": h.Transaction,
+		"openledger.commit":      h.Commit,
 	}
 
-	msgs, err := queue.Channel.Consume(
-		q.Name,    
-		"",  
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
-
-	if err != nil {
-		return err
-	}
-
-	var done chan struct{}
-	go func() {
-		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
+	for queueName, handler := range queues {
+		q, err := h.queue.QueueDeclare(queueName, false, false, false, false, nil)
+		if err != nil {
+			return err
 		}
-	}()
 
-	<-done
+		go func(queueName string, handler func(amqp091.Delivery)) {
+			msgs, err := h.queue.Channel.Consume(
+				q.Name,
+				"",
+				true,
+				false,
+				false,
+				false,
+				nil,
+			)
+			if err != nil {
+				log.Printf("Error while consuming messages from queue %s: %v", q.Name, err)
+				return
+			}
+
+			for d := range msgs {
+				handler(d)
+			}
+		}(queueName, handler)
+	}
+
 	return nil
 }
 
+func (h *ConsumerHandler) Transaction(d amqp091.Delivery) {
+	log.Printf("Processing transaction message: %s", d.Body)
+}
+
+func (h *ConsumerHandler) Commit(d amqp091.Delivery) {
+	log.Printf("Processing commit message: %s", d.Body)
+}
