9856f06ea0d7885ba4eabed4ffd08991ab486ef5
josue santos <josue.santos.dev@gmail.com>
2025-08-10 01:58:33 -0300

    added convertedAt and ConversationRate in Entry domain

--- internal/domain/Entry.go
+++ internal/domain/Entry.go
@@ -4,6 +4,8 @@ type Entry struct {
 	ID        string
 	CreatedAt time.Time
 	UpdatedAt time.Time
+	ConvertedAt time.Time
+	ConversationRate int
 	AccountID string
 	Amount    string
 	Status    string
